3705535d8275a66d373925ed50f3ec6e62726e0e
baraa1999 <baraaalian7@gmail.com>
2023-07-22 08:25:49 +0300

    function in cubit

--- lib/cubit/counter_cubit.dart
+++ lib/cubit/counter_cubit.dart
@@ -3,4 +3,17 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 class CounterCubit extends Cubit<CounterState> {
   CounterCubit(initialState) : super(initialState);
+  int teamAPoints = 0;
+
+  int teamBPoints = 0;
+
+  void TeamAIncrement({required String team, required int buttonNumber}) {
+    if (team == 'A') {
+      teamAPoints += buttonNumber;
+      emit(CounterAIncrementState());
+    } else {
+      teamBPoints += buttonNumber;
+      emit(CounterBIncrementState());
+    }
+  }
 }
