f84b52be0db4d2c558c4ac3b3309a8cd01e8b41b
baraa1999 <baraaalian7@gmail.com>
2023-07-22 08:06:22 +0300

    cubit and states

---
+++ lib/cubit/counter_cubit.dart
@@ -0,0 +1,6 @@
+import 'package:basketball_counter_app/cubit/counter_state.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class CounterCubit extends Cubit<CounterState> {
+  CounterCubit(initialState) : super(initialState);
+}
